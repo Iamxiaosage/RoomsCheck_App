fccf01b27c812e54f14844f0ba74ee01e5182475
Leaf.Iamxiaosage <1445585347@qq.com>
2020-11-15 11:13:58 +0800

    dfsd

--- app/src/main/java/com/example/checkrooms_app/bean/RoomsInfo.kt
+++ app/src/main/java/com/example/checkrooms_app/bean/RoomsInfo.kt
@@ -1,57 +1,57 @@
-package com.example.checkrooms_app.bean
-
-import com.example.checkrooms_app.R
-import kotlinx.android.synthetic.main.item_rv.view.*
-
-/**
- * @author fansx
- * @version $
- * @des
- * @updateAuthor $
- * @updateDes
- */
-//git test11111
-class RoomsInfo{
-    var roomsId:String="000";
-    var isHold:Boolean=false;
-    var isNormal:Boolean=true;
-
-    fun get_IsNormal():String{
-        if (isNormal)return "正常；"
-
-        else return "";
-    }
-
-
-//    fun get_Is
-
-    fun get_IsHold():String {
-        if (isHold)return "有人"
-        else return "";
-    };
-
-    var other:String="";
-
-    var normal:String="正常；";
-
-
-    fun getInfoString():String{
-
-        return  "$normal, $other"
-
-    }
-
-
-
-
-    override fun toString(): String {
-//        return if (isHold) roomsId + ":有人" + "\n"
-//        return  roomsId + get_IsHold()+other+ "\n";
-        return  "$roomsId ： ${get_IsHold()}${get_IsNormal()}$other\n";
-
-
-
-//        else               roomsId + ":" + normal + "\n";
-    }
-
-}
+//package com.example.checkrooms_app.bean
+//
+//import com.example.checkrooms_app.R
+//import kotlinx.android.synthetic.main.item_rv.view.*
+//
+///**
+// * @author fansx
+// * @version $
+// * @des
+// * @updateAuthor $
+// * @updateDes
+// */
+////git test11111
+//class RoomsInfo{
+//    var roomsId:String="000";
+//    var isHold:Boolean=false;
+//    var isNormal:Boolean=true;
+//
+//    fun get_IsNormal():String{
+//        if (isNormal)return "正常；"
+//
+//        else return "";
+//    }
+//
+//
+////    fun get_Is
+//
+//    fun get_IsHold():String {
+//        if (isHold)return "有人"
+//        else return "";
+//    };
+//
+//    var other:String="";
+//
+//    var normal:String="正常；";
+//
+//
+//    fun getInfoString():String{
+//
+//        return  "$normal, $other"
+//
+//    }
+//
+//
+//
+//
+//    override fun toString(): String {
+////        return if (isHold) roomsId + ":有人" + "\n"
+////        return  roomsId + get_IsHold()+other+ "\n";
+//        return  "$roomsId ： ${get_IsHold()}${get_IsNormal()}$other\n";
+//
+//
+//
+////        else               roomsId + ":" + normal + "\n";
+//    }
+//
+//}
